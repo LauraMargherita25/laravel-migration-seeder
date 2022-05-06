5994b4188fc5b8624b33eae12dca1af786816822
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-05-06 15:02:38 +0200

    Created trains table

---
+++ database/migrations/2022_05_06_124025_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 100);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->dateTime('departure_time', 0);
+            $table->dateTime('arrival_time', 0);
+            $table->smallInteger('train_ID');
+            $table->smallInteger('carriages');
+            $table->boolean('in_time');
+            $table->boolean('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
