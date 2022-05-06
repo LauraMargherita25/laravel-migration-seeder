c0e72c686e3cfccec273d4273140a84a54104b88
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-05-06 16:50:33 +0200

    Used Faker to seed

--- database/seeds/TrainSeeder.php
+++ database/seeds/TrainSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Train;
 use Illuminate\Database\Seeder;
+use App\Train;
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
@@ -10,18 +11,51 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $train = new Train();
-        $train->company = 'Italo';
-        $train->departure_station = 'Milano - Stazione Centrale';
-        $train->arrival_station = 'Roma - Tiburtina';
-        $train->departure_time = '2022-01-01 13:00:00';
-        $train->arrival_time = '2022-01-01 17:30:00';
-        $train->train_ID = 5394;
-        $train->carriages = 10;
-        $train->in_time = true;
-        $train->deleted = false;
-        $train->save();
+
+        // $arrTrains = [
+        //     [
+        //         'company' => 
+        //         'departure_station' => 
+        //         'arrival_station' => 
+        //         'departure_time' => 
+        //         'arrival_time' => 
+        //         'train_ID' => 
+        //         'carriages' => 
+        //         'in_time' => 
+        //         'deleted' => false
+        //     ],
+        // ];
+
+        // $train = new Train();
+        // $train->company = 'Italo';
+        // $train->departure_station = 'Milano - Stazione Centrale';
+        // $train->arrival_station = 'Roma - Tiburtina';
+        // $train->departure_time = '2022-01-01 13:00:00';
+        // $train->arrival_time = '2022-01-01 17:30:00';
+        // $train->train_ID = 5394;
+        // $train->carriages = 10;
+        // $train->in_time = true;
+        // $train->deleted = false;
+        // $train->save();
+
+        for ($i = 1; $i < 100 ; $i ++) { 
+            $trainData = [
+                'company'           => $faker->company(),
+                'departure_station' => $faker->city(),
+                'arrival_station'   => $faker->city(),
+                'departure_time'    => $faker->dateTimeThisYear(),
+                'arrival_time'      => $faker->dateTimeThisYear(),
+                'train_ID'          => $faker->randomNumber(4, true),
+                'carriages'         => $faker->randomNumber(2, true),
+                'in_time'           => true,
+                'deleted'           => false,
+            ];
+
+            $train = new Train();
+            $train->fill($trainData);
+            $train->save();
+        }
     }
 }
